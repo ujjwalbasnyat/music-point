11adc448c4451ec925d3885fb66b06353bbb0246
ujjwalbasnyat <asujjwalbasnyat@gmail.com>
2024-04-07 14:57:06 +0545

    Enhancement: Add Carousel into homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { useState } from "react";
+import { CiCircleChevLeft ,CiCircleChevRight } from "react-icons/ci";
 
 function Navbar(){
     return(
@@ -42,38 +41,54 @@ const Navitems = [
 ]
 
 function SimpleSlider(){
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
+    const[currentIndex,setcurrentIndex] = useState(0);
+    const prevSlide =()=>{
+        const isFirstSlide = currentIndex ===0;
+        const newIndex = isFirstSlide ? Slides.length-1 :currentIndex - 1;
+        setcurrentIndex(newIndex);
+    };
+    const nextSlide =()=>{
+        const islastSlide = currentIndex === Slides.length - 1 ;
+        const newIndex = islastSlide ? 0 : currentIndex +1 ;
+        setcurrentIndex(newIndex)
+    }
+    const Slides = [
+        {
+            name: 'Tylor Swift',
+            url : 'https://i.ytimg.com/vi/-BjZmE2gtdo/maxresdefault.jpg'
+
+        },
+        {
+            name : 'Ed sheeran',
+            url :'https://i.ytimg.com/vi/2Vv-BfVoq4g/sddefault.jpg'
+        },
+        {
+            name :'Passenger',
+            url : 'https://i.ytimg.com/vi/RBumgq5yVrA/maxresdefault.jpg'
+        },
+        {
+            name : 'Salena Gomez & Rema',
+            url : 'https://i.ytimg.com/vi/A7ijl8aiGKs/maxresdefault.jpg'
+        },
+        {
+            name : 'Dan Reynolds',
+            url : 'https://www.udiscovermusic.com/wp-content/uploads/2019/05/Imagine-Dragons-Believer-Press-Shot-Eliot-Lee-Hazel-1000x600.jpg'
+        }
+    ]
     return(
         <div>
-            <h1>hero section</h1>
-            <div className="flex">
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-    </div>
+            
+            <div className={`my-10`}>
+                <div style={{backgroundImage:`url(${Slides[currentIndex].url})`}} className={`  h-60 bg-center  rounded-2xl`}><p className={`text-white font-bold text-[30px] mx-10 py-20`}>{Slides[currentIndex].name}</p></div>
+                {/* left arrow */}
+                <div className=" absolute top-[55%] translate-x-0 translate-y-[-500%] left-50 text-3xl text-white cursor-pointer">
+                    <CiCircleChevLeft onClick={prevSlide}/>
+                </div>
+                {/* right arrow */}
+                <div className=" absolute top-[55%] translate-x-0 translate-y-[-500%] right-[345px] text-3xl text-white cursor-pointer">
+                    <CiCircleChevRight onClick={nextSlide}/>
+                </div>
+            </div>
             
         </div>
     )
